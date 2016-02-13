a25c80cb2adb1dc96598c76695fa2436bac1f269
Derek Sakamoto <dmsakamoto@gmail.com>
2016-02-13 12:09:30 -0800

    Added start-kit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Root extends Component {
+  render() {
+    return <div>Hello</div>;
+  }
+}
+
+
+ReactDOM.render(<Root />, document.getElementById("root"));
